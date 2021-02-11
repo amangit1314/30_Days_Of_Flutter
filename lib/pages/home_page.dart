a6747dc7911b59bf4021f38dd3f06f7d85b2356f
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2021-02-11 15:39:53 +0530

    day 9 Code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+          child: Text("Aman"),
         ),
       ),
       drawer: MyDrawer(),
