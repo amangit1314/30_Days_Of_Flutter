f8bd046fb215a839365ed7cc56efe9cc6841a6a8
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2021-02-11 15:34:52 +0530

    Adding functionalities

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:catelog_app/pages/drawer_pages/saved.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -15,18 +16,30 @@ class MyDrawer extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.person),
             title: Text("Your Account"),
+            onTap: () {
+              Saved();
+            },
           ),
           ListTile(
             leading: Icon(Icons.save),
             title: Text("Saved"),
+            onTap: () {
+              Saved();
+            },
           ),
           ListTile(
             leading: Icon(Icons.settings),
             title: Text("Settings"),
+            onTap: () {
+              Saved();
+            },
           ),
           ListTile(
             leading: Icon(Icons.logout),
             title: Text("Logout"),
+            onTap: () {
+              Saved();
+            },
           ),
         ],
       ),
